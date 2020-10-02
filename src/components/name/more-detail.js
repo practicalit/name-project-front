16d01408d0a53554bc1f7e7b90ddbf76b60a67c5
Practical IT <code@thepracticalit.com>
2020-10-02 14:43:31 -0400

    Updating label for adding similar name

--- src/components/name/more-detail.js
+++ src/components/name/more-detail.js
@@ -173,7 +173,7 @@ const AddMoreDeatil = (props) => {
                 <div className="features-icons-icon d-flex">
                   <i className="icon-list m-auto text-primary"></i>
                 </div>
-                <h3>What is the name that you would like to enter?</h3>
+                <h3>What similart name do you know like yours?</h3>
                 <input
                   onChange={(e) => setName(e.target.value)}
                   className="form-control"
