cf90472091f15c43907224cc750b7aad45235193
bethel <you@example.com>
2020-10-11 23:18:05 -0400

    made changes to submit post request to the backend

--- src/components/landing/add-volunteer.js
+++ src/components/landing/add-volunteer.js
@@ -1,15 +1,44 @@
 import React from "react";
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
 
-function AddVolunteer() {
+const AddVolunteer = (props) => {
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [redirect, setRedirect] = useState(false);
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const payload = { first_name, last_name, email };
+      const response = await fetch(
+        `${process.env.REACT_APP_BACK_SERVER}volunteer`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(payload),
+        }
+      );
+
+      console.log(response);
+      setRedirect(true);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
   return (
     <div style={styleApp}>
-      <form>
+      <form onSubmit={onSubmitForm}>
         <h6>Thanks for your valued voulentering</h6>
         <div></div>
         <div>
           <div style={styleInput}>
             <input
               type="text"
+              value={first_name}
+              onChange={(e) => setFirstName(e.target.value)}
               className="form-control"
               id="firstName"
               placeholder="First Name"
@@ -18,6 +47,8 @@ function AddVolunteer() {
           <div style={styleInput}>
             <input
               type="text"
+              value={last_name}
+              onChange={(e) => setLastName(e.target.value)}
               className="form-control"
               id="lastName"
               placeholder="Last Name"
@@ -26,6 +57,8 @@ function AddVolunteer() {
           <div style={styleInput}>
             <input
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="form-control"
               id="email"
               placeholder="Email"
@@ -86,7 +119,7 @@ function AddVolunteer() {
       </form>
     </div>
   );
-}
+};
 const styleInput = {
   margin: "40px 0px 40px",
 };
