4dc4fd92809d105ad69eda86a59a817d3721db97
Practical IT <code@thepracticalit.com>
2020-10-12 15:47:58 -0400

    Fixing the listing of the volunteers to be read from server

--- src/components/landing/volunteer-list.js
+++ src/components/landing/volunteer-list.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function VolunteerList() {
-  const [volunteer, setvolunteer] = useState([]);
-  const getLookups = () => {
+  const [volunteers, setvolunteer] = useState([]);
+  
+  //use effect hook to get initiated on the first round
+  useEffect(() => {
+    getVolunteers();
+    return () => {
+      //cleanup
+    };
+  }, []);
+
+  const getVolunteers = () => {
     axios
       .get(
         `${process.env.REACT_APP_BACK_SERVER}${process.env.REACT_APP_VOLUNTEER_API}`
       )
       .then((response) => {
+        console.log(response.data);
         setvolunteer(response.data);
       })
       .catch((error) => console.error(error.message));
@@ -19,31 +29,19 @@ function VolunteerList() {
       <table className="table table-hover">
         <thead>
           <tr>
-            <th scope="col">#</th>
             <th scope="col">First name</th>
             <th scope="col">Last name</th>
             <th scope="col">Email</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>9/29/2019</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>8/17/2018</td>
-            <td>4</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry the Bird</td>
-            <td>7/15/2016</td>
-            <td>5</td>
-          </tr>
+          {volunteers.map( volunteer => {
+            return (<tr>
+              <td>{volunteer.first_name}</td>
+              <td>{volunteer.last_name}</td>
+              <td>{volunteer.email}</td>
+            </tr>)
+          })}   
         </tbody>
       </table>
     </React.Fragment>
