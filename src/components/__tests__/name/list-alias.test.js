216cf416a7bea990e29409dca7447a72642c6dcf
filimon27 <65411615+filimon27@users.noreply.github.com>
2020-11-12 12:29:06 -0500

    Create list-alias.test.js
    test has done

---
+++ src/components/__tests__/name/list-alias.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ListAlias from '../../../../src/components/name/list-Alias';
+
+it("checks if ListAlias loads successfully", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<ListAlias></ListAlias>, div);
+})
