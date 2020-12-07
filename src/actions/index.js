e67c0e4e0bffb562430eb904f7b29332169c05aa
Practical IT <code@thepracticalit.com>
2020-12-07 08:09:13 -0500

    Redux based state management added for searched name

---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+//when action's logic is getting complex, it is advisable to break it out
+//and move the logic here than adding the logic in the reducer.
+//for now, the action is simple enough to be handled in the reducer.
